fb53f5260eeda8954760f41be218c8174266df5d
Neze2024 <ikembannabueze@gmail.com>
2026-06-10 20:49:33 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import WhatsAppFloat from './components/WhatsAppFloat'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
+function App() {
+  const location = useLocation()
+  
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+      <Footer />
+      <WhatsAppFloat />
+    </>
+  )
+}
+
+export default App
